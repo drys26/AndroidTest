9e012712fc5e1ed2f52889a43cb907d7cb8a43f7
drys26 <dkc4148077@gmail.com>
2017-05-31 18:09:57 +0800

    Added Youtube Data Adapter

---
+++ app/src/main/java/com/example/nexusbond/sampleapp/YoutubeDataAdapter.java
@@ -0,0 +1,19 @@
+package com.example.nexusbond.sampleapp;
+
+import android.content.Context;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.widget.ArrayAdapter;
+
+/**
+ * Created by nexusbond on 31/05/2017.
+ */
+
+public class YoutubeDataAdapter extends ArrayAdapter<YoutubeData> {
+
+    public YoutubeDataAdapter(@NonNull Context context, @LayoutRes int resource, @NonNull YoutubeData[] objects) {
+        super(context, resource, objects);
+    }
+
+
+}
